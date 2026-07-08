3f778b920875fb5a38f9bc88c66dcaae4d895d65
Koustav Samaddar <koustavsamaddar14@gmail.com>
2026-07-08 17:08:15 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.js
@@ -0,0 +1,39 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+        if (this.minStack.length && this.minStack[this.minStack.length-1] < val) {
+            this.minStack.push(this.minStack[this.minStack.length-1]);
+        } else this.minStack.push(val);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length-1]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minStack[this.minStack.length-1];
+    }
+}
