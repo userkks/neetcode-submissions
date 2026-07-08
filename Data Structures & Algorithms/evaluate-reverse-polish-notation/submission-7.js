70622d1d3d1661ac2d7c7ba905332aec8afa7dba
Koustav Samaddar <koustavsamaddar14@gmail.com>
2026-07-08 17:29:52 +0530

    Add: evaluate-reverse-polish-notation - submission-7

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-7.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        const stack = [];
+        const operation = (v2, v1, sign) => {
+            switch(sign){
+                case '+':
+                    return v2 + v1;
+                case '-':
+                    return v1 - v2;
+                case '*':
+                    return v2 * v1;
+                case '/':
+                    const v = v1 / v2;
+                    if (v >= 0) return Math.floor(v);
+                    else return Math.ceil(v);
+            }
+        }
+        for (let i=0; i<tokens.length; i++) {
+            if(!isNaN(tokens[i])) {
+                stack.push(parseInt(tokens[i]));
+            } else {
+                const temp = operation(stack.pop(), stack.pop(), tokens[i]);
+                console.log(temp);
+                stack.push(temp);
+            }
+        }
+        return stack.pop()
+    }
+}
