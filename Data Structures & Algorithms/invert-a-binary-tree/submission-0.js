0adce0d358121b957d50079f3bf87c22eda62371
Koustav Samaddar <koustavsamaddar14@gmail.com>
2026-07-14 21:24:03 +0530

    Add: invert-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-0.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {TreeNode}
+     */
+    invertTree(root) {
+        if (!root) return null;
+        let rightSub = root.right;
+        root.right = this.invertTree(root.left);
+        root.left = this.invertTree(rightSub);
+        return root;
+    }
+}
