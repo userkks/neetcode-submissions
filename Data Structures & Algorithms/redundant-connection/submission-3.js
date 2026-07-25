a61eced84c6cf1cdd127018afb191a26432423d5
Koustav Samaddar <koustavsamaddar14@gmail.com>
2026-07-25 16:18:54 +0530

    Add: redundant-connection - submission-3

---
+++ Data Structures & Algorithms/redundant-connection/submission-3.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[][]} edges
+     * @return {number[]}
+     */
+    findRedundantConnection(edges) {
+        const memory = new Array(edges.length+1).fill(-1);
+        for (let i=0; i<edges.length+1; i++) {
+            memory[i]=i;
+        }
+        const find = (i) => {
+            if (memory[i] !== i) {
+                memory[i] = find(memory[i]);
+            }
+            return memory[i];
+        }
+
+        const union = (i, j) => {
+            const rootI = find(i);
+            const rootJ = find(j);
+            if (rootI === rootJ) return false;
+            memory[rootJ] = i;
+            return true;
+        }
+
+        for (let [i, j] of edges) {
+            if (!union(i, j)) return [i, j];
+        }
+        
+    }
+}
