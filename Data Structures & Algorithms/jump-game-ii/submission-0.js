afc42de12774c3f9ece214589ab088063f635708
Koustav Samaddar <koustavsamaddar14@gmail.com>
2026-08-02 21:29:36 +0530

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    jump(nums) {
+        const memory = new Array(nums.length).fill(Infinity);
+        const reachables = [[nums.length-1, 0]];
+        for (let i=nums.length-2; i>=0; i--) {
+            const jump = nums[i];
+            let minJump = Infinity;
+            for (let j=reachables.length-1; j>=0; j--) {
+                let [ind, jumpCount] = reachables[j];
+                if (ind <= i+jump) minJump = Math.min(minJump, jumpCount+1);
+                else break;
+            }
+            if (minJump !== Infinity) reachables.push([i, minJump]);
+        }
+        return reachables.pop()[1];
+    }
+}
