c858c8b56f5777a8a70e65c6c78bdffd1348b782
Koustav Samaddar <koustavsamaddar14@gmail.com>
2026-08-07 00:36:13 +0530

    Add: counting-bits - submission-5

---
+++ Data Structures & Algorithms/counting-bits/submission-5.js
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number[]}
+     */
+    countBits(n) {
+        const res = new Array(n + 1).fill(0);
+        for (let i = 1; i <= n; i++) {
+            res[i] = res[i >> 1] + (i & 1);
+        }
+        return res;
+    }
+}
