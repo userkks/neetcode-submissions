1218f5dff7851ea0b59a7a4738a2a6b27a435d2f
Koustav Samaddar <koustavsamaddar14@gmail.com>
2026-08-11 18:12:13 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let res = "";
+        for (let st of strs) {
+            res += `${st.length}#${st}`;
+        }
+        return res;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let i = 0;
+        let start = 0;
+        const res = [];
+        while (i < str.length) {
+            if (str[i] === "#") {
+                const len = Number(str.slice(start, i));
+                res.push(str.slice(i + 1, i + 1 + len));
+                start = i + 1 + len;
+                i = i + len;
+            }
+            i++;
+        }
+        return res;
+    }
+}
