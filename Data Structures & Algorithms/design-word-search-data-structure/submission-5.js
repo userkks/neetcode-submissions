c60148620eea163ef1d010c68b6eb73880aeb597
Koustav Samaddar <koustavsamaddar14@gmail.com>
2026-08-23 17:15:59 +0530

    Add: design-word-search-data-structure - submission-5

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-5.js
@@ -0,0 +1,50 @@
+class Trie {
+    constructor() {
+        this.end = false;
+        this.next = {};
+    }
+    insert(word) {
+        let curNode = this;
+        for (let l of word) {
+            if (!(l in curNode.next)) curNode.next[l] = new Trie();
+            curNode = curNode.next[l];
+        }
+        curNode.end = true;
+    }
+    search(word) {
+        let curNodeList = [this];
+        for (let l of word) {
+            const tempNodeList = [];
+            if (l === ".") {
+                curNodeList.forEach((n) => tempNodeList.push(...Object.values(n.next)));
+            } else {
+                const nodesReachable = curNodeList.map((n) => n.next[l]).filter((i) => i);
+                tempNodeList.push(...nodesReachable);
+            }
+            curNodeList = tempNodeList;
+        }
+        return curNodeList.some((i) => i.end);
+    }
+}
+
+class WordDictionary {
+    constructor() {
+        this.trie = new Trie();
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    addWord(word) {
+        this.trie.insert(word);
+    }
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word) {
+        return this.trie.search(word);
+    }
+}
