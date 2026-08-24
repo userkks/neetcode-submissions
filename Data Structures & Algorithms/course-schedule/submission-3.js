e030b59824d4e4fcaa623c09932041360d863c4c
Koustav Samaddar <koustavsamaddar14@gmail.com>
2026-08-24 14:06:00 +0530

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses, prerequisites) {
+        const inDegree = new Array(numCourses).fill(0);
+        const graph = Array.from({ length: numCourses }, () => []);
+        for (let [j, i] of prerequisites) {
+            inDegree[j]++;
+            graph[i].push(j);
+        }
+        const queue = [];
+        for (let i = 0; i < inDegree.length; i++) {
+            if (inDegree[i] === 0) queue.push(i);
+        }
+        let head = 0;
+        const order = [];
+        while (head < queue.length) {
+            const node = queue[head++];
+            order.push(node);
+            for (let nei of graph[node]) {
+                inDegree[nei]--;
+                if (inDegree[nei] === 0) queue.push(nei);
+            }
+        }
+        return order.length === numCourses;
+    }
+}
